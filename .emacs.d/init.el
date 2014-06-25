1722181e70886443e3ad142054789245cc76d277
HAYAMI Ken'ichi <noname@example.jp>
2014-06-25 22:41:02 +0900

    none

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,20 +1,22 @@
 ;;;		-*- Mode: Emacs-Lisp; coding: utf-8; indent-tabs-mode: nil -*-
 ;;;
-;;;	まず最初に、system-name を FQDN ではなく short name にする
-;;;	(frame title に FQDN が表示されなくする)
+;;; ~/.emacs.d/init.el
+
 ;;;
-;;;	The symbol system-name is a variable as well as a function.
-;;;	In fact, the function returns whatever value the variable
-;;;	system-name currently holds. Thus, you can set the variable
-;;;	system-name in case Emacs is confused about the name of your
-;;;	system. The variable is also useful for constructing frame
-;;;	titles (see Frame Titles). 
+;;; まず最初に、system-name を FQDN ではなく short name にする
+;;; (frame title に FQDN が表示されなくする)
+;;;
+;;; The symbol system-name is a variable as well as a function. In fact, the
+;;; function returns whatever value the variable system-name currently holds.
+;;; Thus, you can set the variable system-name in case Emacs is confused about
+;;; the name of your system. The variable is also useful for constructing frame
+;;; titles (see Frame Titles).
 ;;;
 (setq system-name (car (split-string system-name "\\.")))
 
 
 ;;;
-;;;	site-lisp とそのサブディレクトリを load-path に加える
+;;; site-lisp とそのサブディレクトリを load-path に加える
 ;;;
 (let ((dir "/usr/local/share/emacs/site-lisp"))
   (when (and
@@ -26,7 +28,7 @@
 
 
 ;;;
-;;;	$HOME/sys/local/share/emacs/site-lisp があれば load-path に加える
+;;; ~/sys/local/share/emacs/site-lisp があれば load-path に加える
 ;;;
 (let ((dir (expand-file-name "~/sys/local/share/emacs/site-lisp")))
   (when (and
@@ -38,7 +40,7 @@
 
 
 ;;;
-;;;	与えられたリストから basename が一致するディレクトリのリストを返す
+;;; 与えられたリストから basename が一致するディレクトリのリストを返す
 ;;;
 (defun basename-match (basename directory-path)
   (let ((list nil))
@@ -52,7 +54,7 @@
 
 ;;;
 ;;;
-;;;	GNU Emacs 24.x 用各種設定
+;;; 各種設定
 ;;;
 (load-file "~/.emacs.d/local-lisp/ja-env.el")
 (load-file "~/.emacs.d/local-lisp/keybind.el")
@@ -67,4 +69,4 @@
 (load-file "~/.emacs.d/local-lisp/misc.el")
 
 
-;;; EOF
+;;; ~/.emacs.d/init.el ends here
