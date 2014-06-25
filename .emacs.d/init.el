c6135625031dae88ef72f6ff0b8361c4f017cc4a
HAYAMI Ken'ichi <noname@example.jp>
2014-06-25 23:08:36 +0900

    none

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,7 +14,6 @@
 ;;;
 (setq system-name (car (split-string system-name "\\.")))
 
-
 ;;;
 ;;; site-lisp とそのサブディレクトリを load-path に加える
 ;;;
@@ -25,7 +24,6 @@
     (add-to-list 'load-path dir)
     (let ((default-directory dir))
       (normal-top-level-add-subdirs-to-load-path))))
-
 
 ;;;
 ;;; ~/sys/local/share/emacs/site-lisp があれば load-path に加える
@@ -38,27 +36,14 @@
     (let ((default-directory dir))
       (normal-top-level-add-subdirs-to-load-path))))
 
-
-;;;
-;;; 与えられたリストから basename が一致するディレクトリのリストを返す
-;;;
-(defun basename-match (basename directory-path)
-  (let ((list nil))
-    (dolist (path directory-path)
-      (setq dir (car (last (split-string path "/"))))
-      (cond ((and (string= basename dir)
-                  (file-accessible-directory-p path))
-             (add-to-list 'list path t nil))))
-    (eval 'list)))
-
-
-;;;
 ;;;
 ;;; 各種設定
 ;;;
 (load-file "~/.emacs.d/local-lisp/ja-env.el")
 (load-file "~/.emacs.d/local-lisp/keybind.el")
-(cond ((basename-match "wnn7" load-path)
+;(setq wnn7-server-list '("localhost"))
+;(setq wnn7-server-user "guest")	; local feature
+(cond ((require 'wnn7egg-leim nil t)
        (load-file "~/.emacs.d/local-lisp/wnn7-egg.el")))
 (cond (window-system
        (load-file "~/.emacs.d/local-lisp/x-client.el")))
@@ -67,6 +52,5 @@
 (load-file "~/.emacs.d/local-lisp/shell-mode.el")
 (load-file "~/.emacs.d/local-lisp/cc-mode.el")
 (load-file "~/.emacs.d/local-lisp/misc.el")
-
 
 ;;; ~/.emacs.d/init.el ends here
