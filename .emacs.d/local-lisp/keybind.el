a7a93a5576525034f2f1d68854aff9dc7fdf3323
HAYAMI Ken'ichi <noname@example.jp>
2014-06-26 23:58:49 +0900

    none

--- .emacs.d/local-lisp/keybind.el
+++ .emacs.d/local-lisp/keybind.el
@@ -1,25 +1,18 @@
 ;;;		-*- Mode: Emacs-Lisp; coding: utf-8; indent-tabs-mode: nil -*-
 ;;;
-;;;	Key Bindings
-;;;
-
-;;; BackSpace (C-h) と Delete キーを交換
-;;; Emacs 21.x では交換する必要はないのだが、各種モードで
-;;; C-h がヘルプに割り当てられているため、これで回避する。
-;(keyboard-translate ?\C-h ?\C-?)
-;(keyboard-translate ?\C-? ?\C-h)
+;;; keybind.el
 
 ;;; C-h で BackSpace
-(global-set-key "\C-h" 'backward-delete-char)
+(global-set-key (kbd "C-h") 'backward-delete-char)
 
 ;;; M-? [?abc...] でヘルプ
-(global-set-key "\M-?" 'help-command)
+(global-set-key (kbd "M-?") 'help-command)
 
 ;;; Mark set は C-SPC から M-SPC に変更
 ;;; C-SPC は、仮名漢字変換用のキーとして確保
-(global-set-key "\M- " 'set-mark-command)
+(global-set-key (kbd "M-SPC") 'set-mark-command)
 
 ;;; C-x C-b でバッファーメニューのセレクト
-(global-set-key "\C-x\C-b" 'buffer-menu)
+(global-set-key (kbd "C-x C-b") 'buffer-menu)
 
-;;; EOF
+;;; keybind.el ends here
