cebbb897f265558c26d040fdbf6cc1f5e8203510
HAYAMI <noname@example.com>
2025-11-11 21:37:47 +0900

    update customize-omni.sh

--- misc/customize-omni.sh
+++ misc/customize-omni.sh
@@ -6,15 +6,20 @@ umask 022
 # you may want to try deleting the scriptCache*.bin files in the
 # %LOCALAPPDATA%\Mozilla\Firefox\Profiles\(profile)\startupCache\ directory.
 #topdir='/mnt/c/Program Files/Mozilla Firefox'
-topdir='/snap/firefox'
+
+# for Linux: The scriptCache*.bin files mentioned above are updated when
+# Firefox is launched with the 'firefox --purgecaches' (A similar option
+# may exist on Windows, but this has not been confirmed).
+topdir="/usr/lib/firefox"
 
 
 tmpdir=$(mktemp -d --tmpdir omni.XXXXXXXXXX)
-xhtmlpath=chrome/browser/content/browser/browser.xhtml
-xhtmlname=$(basename $xhtmlpath)
+targetpath=chrome/browser/content/browser/browser.xhtml
+targetfile=$(basename $targetpath)
 
 
-substitute() (
+# customize browser.xhtml
+customize() (
     LAST_tag=
     LAST_id=
     LAST_modifiers=
@@ -73,15 +78,37 @@ substitute() (
 )
 
 
-n=0
-find "$topdir" ! -type l -type f -name omni.ja -printf "%P\n" | sort -u | while read omnipath; do
-    n=$(($n + 1))
-    xd=$tmpdir/extract.$n
-    mkdir $xd
+diffcolorless() (
+    dh=/usr/share/doc/git/contrib/diff-highlight/diff-highlight
+    if [ -f "$dh" ]; then
+            env GIT_CONFIG_COUNT=2					\
+                GIT_CONFIG_KEY_0=color.diff-highlight.oldHighlight	\
+                GIT_CONFIG_VALUE_0='red bold'				\
+                GIT_CONFIG_KEY_1=color.diff-highlight.newHighlight	\
+                GIT_CONFIG_VALUE_1='green bold'				\
+                perl "$dh"						\
+            < "$1" | less -R
+    else
+            less "$1"
+    fi
+)
+
+
+exitret=0
+for omnipath in omni.ja browser/omni.ja; do
+
+    if [ ! -f $topdir/$omnipath ]; then
+        echo "ERROR: file not found: $topdir/$omnipath" 1>&2
+        exitret=1
+        continue
+    fi
+
+    xd=$tmpdir/${omnipath}--extract.d
+    mkdir -p $xd
 
     #echo "Extracting: $topdir/$omnipath"
     ret=0
-    unzip -q "$topdir/$omnipath" -d $xd > /dev/null 2>&1 || ret=$?
+    unzip -q $topdir/$omnipath -d $xd > /dev/null 2>&1 || ret=$?
     #
     # Seems fine for the following warnings and errors.
     #   > warning [../omni.ja]:  43553032 extra bytes at beginning or within zipfile
@@ -91,62 +118,62 @@ find "$topdir" ! -type l -type f -name omni.ja -printf "%P\n" | sort -u | while 
     #   >   zipfile?).  Compensating...
     #
     if [ $ret -gt 2 ]; then
-        echo "Failed to unzip: $topdir/$omnipath" 1>&2
+        echo "ERROR: failed to unzip: $topdir/$omnipath" 1>&2
+        rm -rf $xd
+        exitret=1
+        continue
+    fi
+
+    if [ ! -f $xd/$targetpath ]; then
+        # browser.xhtml is not in omni.ja
+        echo "ERROR: $targetfile is not found in $topdir/$omnipath" 1>&2
+        rm -rf $xd
+        exitret=1
+        continue
+    fi
+
+    cp -p $xd/$targetpath $xd/$targetpath.orig
+    customize $xd/$targetpath.orig > $xd/$targetpath.new
+    lines=$(diff -u $xd/$targetpath $xd/$targetpath.new | wc -l)
+    if [ $lines = 0 ]; then
+        # browser.xhtml is edited but the result is the same as before
+        echo "NOTICE: No text to edit in $targetfile in $topdir/$omnipath" 1>&2
+        echo "NOTICE: or the $targetfile has already been customized. (skipping)" 1>&2
         rm -rf $xd
         continue
     fi
 
-    if [ ! -f $xd/$xhtmlpath ]; then
-        # browser.xhtml is not in omni.ja
-        echo "Notice: $xhtmlname is not found in $topdir/$omnipath (skipping)" 1>&2
-        continue;
-    fi
+    cp -p $xd/$targetpath.orig $tmpdir/${omnipath}--$targetfile.orig
+    cp -p $xd/$targetpath.new $tmpdir/${omnipath}--$targetfile.new
+    diff -u $xd/$targetpath.orig $xd/$targetpath.new > $tmpdir/${omnipath}--$targetfile.diff || :
 
-    cp -pi $xd/$xhtmlpath $xd/$xhtmlpath.orig
-    substitute $xd/$xhtmlpath.orig > $xd/$xhtmlpath.new
-    lines=$(diff -u $xd/$xhtmlpath $xd/$xhtmlpath.new | wc -l)
-    if [ $lines = 0 ]; then
-        # browser.xhtml is edited but the result is the same as before
-        echo "Notice: no matches found in $xhtmlname in $topdir/$omnipath (skipping)" 1>&2
-        continue
-    fi
+    cat $xd/$targetpath.new > $xd/$targetpath
+    rm -f $xd/$targetpath.new
+    rm -f $xd/$targetpath.orig
 
-    mkdir -p $tmpdir/$(dirname $omnipath)
-    cp -pi $xd/$xhtmlpath.orig $tmpdir/$omnipath" ($xhtmlname)".orig
-    cp -pi $xd/$xhtmlpath.new  $tmpdir/$omnipath" ($xhtmlname)".new
-    diff -u $xd/$xhtmlpath.orig \
-            $xd/$xhtmlpath.new > $tmpdir/$omnipath" ($xhtmlname)".diff || :
-
-    cat $xd/$xhtmlpath.new > $xd/$xhtmlpath
-    rm -f $xd/$xhtmlpath.new
-    rm -f $xd/$xhtmlpath.orig
-
-    cp -pi "$topdir/$omnipath" $tmpdir/$omnipath.orig
-    cp -pi "$topdir/$omnipath" $tmpdir/$omnipath
-
+    cp -p $topdir/$omnipath $tmpdir/$omnipath.orig
+    cp -p $topdir/$omnipath $tmpdir/$omnipath
     (cd $xd && zip -0DXqr - *) > $tmpdir/$omnipath.new
 
     ret=0
     cmp --quiet "$topdir/$omnipath" $tmpdir/$omnipath.new || ret=$?
     if [ $ret -eq 0 ]; then
-        # omni.ja is modified but the result is the same as before
-        echo "Notice: updated $topdir/$omnipath is identical to the original file (skipping)" 1>&2
+        # omni.ja is updated but the result is the same as before
+        echo "NOTICE: The updated file is no difference from before. (skipping)" 1>&2
+        rm -rf $xd
         continue
     fi
     if [ $ret -eq 1 ]; then
-        # omni.ja is modified and is different from the previous contents
-        (cat $tmpdir/$omnipath.new > $tmpdir/$omnipath) 2> /dev/null || {
-            chmod u+w $tmpdir/$omnipath
-            cat $tmpdir/$omnipath.new > $tmpdir/$omnipath
-            chmod u-w $tmpdir/$omnipath
-        }
+        # omni.ja is updated and is different from the previous contents
+        cat $tmpdir/$omnipath.new > $tmpdir/$omnipath
         rm -f $tmpdir/$omnipath.new
 
+        diffcolorless $tmpdir/${omnipath}--$targetfile.diff
         echo "Updated file created: $tmpdir/$omnipath"
     fi
+    rm -rf $xd
 done
 
-rm -rf $tmpdir/extract.*
 rmdir $tmpdir 2> /dev/null || :
 
-exit 0
+exit $exitret
